873331985ab77579714dc45c0e93ffde7080cab1
Apurv Sethi <as2658@cornell.edu>
2016-03-13 16:26:15 -0400

    GameCanvas, GameEngine w/out loadingMode from labs, new Gameplay Controller, #1

--- MountSputnik/core/src/beigegang/mountsputnik/GameEngine.java
+++ MountSputnik/core/src/beigegang/mountsputnik/GameEngine.java
@@ -2,26 +2,45 @@ package beigegang.mountsputnik;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameEngine extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	/** AssetManager to load game assets (textures, sounds, etc.) */
+	AssetManager manager;
+	
+	/** Drawing context to display graphics (VIEW CLASS) */
+	GameCanvas  canvas;
+	/** GameplayController to set up and play the game */
+	GameplayController controller;
+	
+	public GameEngine() {}
 	
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		manager = new AssetManager();
+		canvas = new GameCanvas();
+		controller = new GameplayController();
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render() {
+		// Update the game state
+		controller.update();
+		
+		// Draw the game
+		Gdx.gl.glClearColor(0.39f, 0.58f, 0.93f, 1.0f);  // Homage to the XNA years
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		canvas.begin();
+		controller.draw(canvas);
+		canvas.end();
+	}
+	
+	@Override
+	public void dispose() {
+		GameplayController.UnloadContent(manager);
+		manager.clear();
+		manager.dispose();
+		manager = null;
 	}
 }
---
+++ MountSputnik/core/src/beigegang/mountsputnik/GameplayController.java
@@ -0,0 +1,34 @@
+package beigegang.mountsputnik;
+
+import com.badlogic.gdx.assets.AssetManager;
+
+public class GameplayController {
+	
+	public static void PreLoadContent(AssetManager manager) {
+		
+	}
+	
+	public static void LoadContent(AssetManager manager) {
+		
+	}
+	
+	public static void UnloadContent(AssetManager manager) {
+		
+	}
+	
+	public GameplayController() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void draw(GameCanvas view) {
+		
+	}
+	
+	public void dispose() {
+		
+	}
+}
