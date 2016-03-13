69935c2e414c7d9853bb0e511f5a5c94dffde958
Apurv Sethi <as2658@cornell.edu>
2016-03-13 13:54:56 -0400

    Add basic libgdx setup, #1

---
+++ MountSputnik/desktop/src/beigegang/mountsputnik/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package beigegang.mountsputnik.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import beigegang.mountsputnik.GameEngine;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameEngine(), config);
+	}
+}
