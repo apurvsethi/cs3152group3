274b8a4951d422bfa998858385c65fb67db194c0
Apurv Sethi <as2658@cornell.edu>
2016-03-13 16:24:32 -0400

    Don't let screen be resizable (for now), #1

--- MountSputnik/desktop/src/beigegang/mountsputnik/desktop/DesktopLauncher.java
+++ MountSputnik/desktop/src/beigegang/mountsputnik/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import beigegang.mountsputnik.GameEngine;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.resizable = false;
 		new LwjglApplication(new GameEngine(), config);
 	}
 }
