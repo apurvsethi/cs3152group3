aa6d43b02cec8723044257b89b5b734bc85c6db7
Apurv Sethi <as2658@cornell.edu>
2016-03-16 02:05:19 -0400

    #1 Added mode controller for all modes of game, temporary menu and pause modes

---
+++ MountSputnik/core/src/beigegang/mountsputnik/ModeController.java
@@ -0,0 +1,537 @@
+/*
+ * ModeController.java
+ *
+ * We will talk about the concept of a Player Mode the first few weeks
+ * of class. A player mode is often like a self-contained game in
+ * its own right. Therefore, it should have its own root (sub)controller.
+ * Since the game will often switch back and forth between player modes
+ * we would like the modes to have a uniform interface.  That way they
+ * can all be handled the same by the true root controller, GDXRoot.
+ *
+ * Author: Walker M. White
+ * Based on original GameX Ship Demo by Rama C. Hoetzlein, 2002
+ * LibGDX version, 1/16/2015
+ */
+package beigegang.mountsputnik;
+
+import java.util.Iterator;
+
+import beigegang.util.FilmStrip;
+import beigegang.util.PooledList;
+import beigegang.util.ScreenListener;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
+
+/**
+ * Interface for the root class of a player mode.
+ *
+ * Each player mode is a game to itself and so it needs an update and a
+ * draw cycle.  The class constructor allocates all necessary resources,
+ * but the class must have an explicit dispose() to release resources
+ * (do not depend on garbage collection)
+ */
+public abstract class ModeController implements Screen {
+	
+	/** 
+	 * Tracks the asset state.  Otherwise subclasses will try to load assets 
+	 */
+	protected enum AssetState {
+		/** No assets loaded */
+		EMPTY,
+		/** Still loading assets */
+		LOADING,
+		/** Assets are complete */
+		COMPLETE
+	}
+
+	/** Track asset loading from all instances and subclasses */
+	protected AssetState worldAssetState = AssetState.EMPTY;
+	/** Track all loaded assets (for unloading purposes) */
+	protected Array<String> assets;	
+	
+	/**
+	 * Preloads the assets for this controller.
+	 *
+	 * Opted for nonstatic loaders, but still want the assets themselves to be 
+	 * static. So AssetState determines the current loading state, only load if
+	 * assets are not already loaded.
+	 * 
+	 * @param manager Reference to global asset manager.
+	 */
+	public void preLoadContent(AssetManager manager) {
+		// Load shared assets as necessary
+	}
+	
+	/**
+	 * Loads the assets for this controller.
+	 *
+	 * Opted for nonstatic loaders, but still want the assets themselves to be 
+	 * static. So AssetState determines the current loading state, only load if
+	 * assets are not already loaded.
+	 * 
+	 * @param manager Reference to global asset manager.
+	 */
+	public void loadContent(AssetManager manager) {
+		// Load shared assets as necessary
+	}
+	
+	/**
+	 * Returns a newly loaded texture region for the given file.
+	 *
+	 * This helper methods is used to set texture settings (such as scaling, and
+	 * whether or not the texture should repeat) after loading.
+	 *
+	 * @param manager 	Reference to global asset manager.
+	 * @param file		The texture (region) file
+	 * @param repeat	Whether the texture should be repeated
+	 *
+	 * @return a newly loaded texture region for the given file.
+	 */
+	protected TextureRegion createTexture(AssetManager manager, String file, boolean repeat) {
+		if (manager.isLoaded(file)) {
+			TextureRegion region = new TextureRegion(manager.get(file, Texture.class));
+			region.getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+			if (repeat) {
+				region.getTexture().setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
+			}
+			return region;
+		}
+		return null;
+	}
+	
+	/**
+	 * Returns a newly loaded filmstrip for the given file.
+	 *
+	 * This helper methods is used to set texture settings (such as scaling, and
+	 * the number of animation frames) after loading.
+	 *
+	 * @param manager 	Reference to global asset manager.
+	 * @param file		The texture (region) file
+	 * @param rows 		The number of rows in the filmstrip
+	 * @param cols 		The number of columns in the filmstrip
+	 * @param size 		The number of frames in the filmstrip
+	 *
+	 * @return a newly loaded texture region for the given file.
+	 */
+	protected FilmStrip createFilmStrip(AssetManager manager, String file, int rows, int cols, int size) {
+		if (manager.isLoaded(file)) {
+			FilmStrip strip = new FilmStrip(manager.get(file, Texture.class),rows,cols,size);
+			strip.getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+			return strip;
+		}
+		return null;
+	}
+	
+	/** 
+	 * Unloads the assets for this game.
+	 * 
+	 * This method erases the static variables.  It also deletes the associated textures 
+	 * from the asset manager. If no assets are loaded, this method does nothing.
+	 * 
+	 * @param manager Reference to global asset manager.
+	 */
+	public void unloadContent(AssetManager manager) {
+		for (String s : assets)
+			if (manager.isLoaded(s)) manager.unload(s);
+	}
+	
+	/** Exit code for starting new level */
+	public static final int EXIT_GAME_NEW = 0;
+	/** Exit code for quitting to menu */
+	public static final int EXIT_MENU = 1;
+	/** Exit code for resuming game */
+	public static final int EXIT_GAME_RESUME = 2;
+	/** Exit code for pause */
+	public static final int EXIT_PAUSE = 3;
+	/** Exit code for quitting the game */
+	public static final int EXIT_QUIT = 4;
+
+	/** The amount of time for a physics engine step. */
+	public static final float WORLD_STEP = 1/60.0f;
+	/** Number of velocity iterations for the constrain solvers */
+	public static final int WORLD_VELOC = 6;
+	/** Number of position iterations for the constrain solvers */
+	public static final int WORLD_POSIT = 2;
+	
+	/** Width of the game world in Box2d units */
+	protected static final float DEFAULT_WIDTH  = 32.0f;
+	/** Height of the game world in Box2d units */
+	protected static final float DEFAULT_HEIGHT = 18.0f;
+	/** The default value of gravity (going down) */
+	protected static final float DEFAULT_GRAVITY = -4.9f;
+
+	/** Reference to the game canvas */
+	protected GameCanvas canvas;
+	/** All the objects in the world. */
+	protected PooledList<GameObject> objects  = new PooledList<GameObject>();
+	/** Queue for adding objects */
+	protected PooledList<GameObject> addQueue = new PooledList<GameObject>();
+	/** Listener that will update the player mode when we are done */
+	protected ScreenListener listener;
+
+	/** The Box2D world */
+	protected World world;
+	/** The boundary of the world */
+	protected Rectangle bounds;
+	/** The world scale */
+	protected Vector2 scale;
+	
+	/** Whether or not this is an active controller */
+	protected boolean active;
+	/** Whether or not debug mode is active */
+	protected boolean debug;
+
+	/**
+	 * Returns true if debug mode is active.
+	 *
+	 * If true, all objects will display their physics bodies.
+	 *
+	 * @return true if debug mode is active.
+	 */
+	public boolean isDebug( ) {
+		return debug;
+	}
+
+	/**
+	 * Sets whether debug mode is active.
+	 *
+	 * If true, all objects will display their physics bodies.
+	 *
+	 * @param value whether debug mode is active.
+	 */
+	public void setDebug(boolean value) {
+		debug = value;
+	}
+
+	/**
+	 * Returns true if this is the active screen
+	 *
+	 * @return true if this is the active screen
+	 */
+	public boolean isActive( ) {
+		return active;
+	}
+
+	/**
+	 * Returns the canvas associated with this controller
+	 *
+	 * The canvas is shared across all controllers
+	 *
+	 * @param the canvas associated with this controller
+	 */
+	public GameCanvas getCanvas() {
+		return canvas;
+	}
+	
+	/**
+	 * Sets the canvas associated with this controller
+	 *
+	 * The canvas is shared across all controllers.  Setting this value will compute
+	 * the drawing scale from the canvas size.
+	 *
+	 * @param value the canvas associated with this controller
+	 */
+	public void setCanvas(GameCanvas canvas) {
+		this.canvas = canvas;
+		this.scale.x = canvas.getWidth()/bounds.getWidth();
+		this.scale.y = canvas.getHeight()/bounds.getHeight();
+	}
+
+	/**
+	 * Creates a new game world with the default values.
+	 *
+	 * The game world is scaled so that the screen coordinates do not agree
+	 * with the Box2d coordinates.  The bounds are in terms of the Box2d
+	 * world, not the screen.
+	 */
+	protected ModeController() {
+		this(new Rectangle(0,0,DEFAULT_WIDTH,DEFAULT_HEIGHT), 
+			 new Vector2(0,DEFAULT_GRAVITY));
+	}
+
+	/**
+	 * Creates a new game world
+	 *
+	 * The game world is scaled so that the screen coordinates do not agree
+	 * with the Box2d coordinates.  The bounds are in terms of the Box2d
+	 * world, not the screen.
+	 *
+	 * @param width  	The width in Box2d coordinates
+	 * @param height	The height in Box2d coordinates
+	 * @param gravity	The downward gravity
+	 */
+	protected ModeController(float width, float height, float gravity) {
+		this(new Rectangle(0,0,width,height), new Vector2(0,gravity));
+	}
+
+	/**
+	 * Creates a new game world
+	 *
+	 * The game world is scaled so that the screen coordinates do not agree
+	 * with the Box2d coordinates.  The bounds are in terms of the Box2d
+	 * world, not the screen.
+	 *
+	 * @param bounds	The game bounds in Box2d coordinates
+	 * @param gravity	The gravitational force on this Box2d world
+	 */
+	protected ModeController(Rectangle bounds, Vector2 gravity) {
+		assets = new Array<String>();
+		world = new World(gravity,false);
+		objects = new PooledList<GameObject>();
+		this.bounds = new Rectangle(bounds);
+		this.scale = new Vector2(1,1);
+		debug  = false;
+		active = false;
+	}
+
+	/**
+	 *
+	 * Adds a physics object in to the insertion queue.
+	 *
+	 * Objects on the queue are added just before collision processing.  We do this to 
+	 * control object creation.
+	 *
+	 * param obj The object to add
+	 */
+	public void addQueuedObject(GameObject obj) {
+		assert inBounds(obj) : "Object is not in bounds";
+		addQueue.add(obj);
+	}
+
+	/**
+	 * Immediately adds the object to the physics world
+	 *
+	 * param obj The object to add
+	 */
+	protected void addObject(GameObject obj) {
+		assert inBounds(obj) : "Object is not in bounds";
+		objects.add(obj);
+		obj.activatePhysics(world);
+	}
+
+	/**
+	 * Returns true if the object is in bounds.
+	 *
+	 * This assertion is useful for debugging the physics.
+	 *
+	 * @param obj The object to check.
+	 *
+	 * @return true if the object is in bounds.
+	 */
+	public boolean inBounds(GameObject obj) {
+		boolean horiz = (bounds.x <= obj.getX() && obj.getX() <= bounds.x+bounds.width);
+		boolean vert  = (bounds.y <= obj.getY() && obj.getY() <= bounds.y+bounds.height);
+		return horiz && vert;
+	}
+	
+	/**
+	 * Resets the status of the game so that we can play again.
+	 *
+	 * This method disposes of the world and creates a new one.
+	 */
+	public abstract void reset();
+	
+	/**
+	 * Returns whether to process the update loop
+	 *
+	 * At the start of the update loop, we check if it is time
+	 * to switch to a new game mode.  If not, the update proceeds
+	 * normally.
+	 *
+	 * @param delta Number of seconds since last animation frame
+	 * 
+	 * @return whether to process the update loop
+	 */
+	public boolean preUpdate(float dt) {
+		InputController input = InputController.getInstance();
+		input.readInput(bounds, scale);
+		if (listener == null) {
+			return true;
+		}
+
+		// Toggle debug
+		if (input.didDebug()) {
+			debug = !debug;
+		}
+		
+		// Now it is time to maybe switch screens.
+		if (input.didMenu()) {
+			listener.exitScreen(this, EXIT_QUIT);
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * The core gameplay loop of this world.
+	 *
+	 * This method contains the specific update code for this screen. It does
+	 * not handle collisions, as those are managed by the parent class ModeController.
+	 * This method is called after input is read, but before collisions are resolved.
+	 * The very last thing that it should do is apply forces to the appropriate objects.
+	 *
+	 * @param delta Number of seconds since last animation frame
+	 */
+	public abstract void update(float dt);
+	
+	/**
+	 * Processes physics
+	 *
+	 * Once the update phase is over, but before we draw, we are ready to handle
+	 * physics.  The primary method is the step() method in world.  This implementation
+	 * works for all applications and should not need to be overwritten.
+	 *
+	 * @param delta Number of seconds since last animation frame
+	 */
+	public void postUpdate(float dt) {
+		// Add any objects created by actions
+		while (!addQueue.isEmpty()) {
+			addObject(addQueue.poll());
+		}
+		
+		// Turn the physics engine crank.
+		world.step(WORLD_STEP,WORLD_VELOC,WORLD_POSIT);
+
+		// Garbage collect the deleted objects.
+		// Note how we use the linked list nodes to delete O(1) in place.
+		// This is O(n) without copying.
+		Iterator<PooledList<GameObject>.Entry> iterator = objects.entryIterator();
+		while (iterator.hasNext()) {
+			PooledList<GameObject>.Entry entry = iterator.next();
+			GameObject obj = entry.getValue();
+			if (obj.isRemoved()) {
+				obj.deactivatePhysics(world);
+				entry.remove();
+			} else {
+				// Note that update is called last!
+				obj.update(dt);
+			}
+		}
+	}
+	
+	/**
+	 * Draw the physics objects to the canvas
+	 *
+	 * For simple worlds, this method is enough by itself.  It will need
+	 * to be overriden if the world needs fancy backgrounds or the like.
+	 *
+	 * The method draws all objects in the order that they were added.
+	 *
+	 * @param canvas The drawing context
+	 */
+	public void draw() {
+		canvas.clear();
+		
+		canvas.begin();
+		for(GameObject obj : objects) {
+			obj.draw(canvas);
+		}
+		canvas.end();
+		
+		if (debug) {
+			canvas.beginDebug();
+			for(GameObject obj : objects) {
+				obj.drawDebug(canvas);
+			}
+			canvas.endDebug();
+		}
+	}
+	
+	/**
+	 * Dispose of all (non-static) resources allocated to this mode.
+	 */
+	public void dispose() {
+		for(GameObject obj : objects) 
+			obj.deactivatePhysics(world);
+		objects.clear();
+		addQueue.clear();
+		world.dispose();
+		objects = null;
+		addQueue = null;
+		bounds = null;
+		scale  = null;
+		world  = null;
+		canvas = null;
+	}
+
+	/**
+	 * Resize the window for this player mode to the given dimensions.
+	 *
+	 * This method is not guaranteed to be called when the player mode
+	 * starts.  If the window size is important to the player mode, then
+	 * these values should be passed to the constructor at start.
+	 *
+	 * @param width The width of the game window
+	 * @param height The height of the game window
+	 */
+	public abstract void resize(int width, int height);
+	
+	/**
+	 * Called when the Screen should render itself.
+	 *
+	 * We defer to the other methods update() and draw().  However, it is VERY important
+	 * that we only quit AFTER a draw.
+	 *
+	 * @param delta Number of seconds since last animation frame
+	 */
+	public void render(float delta) {
+		if (active) {
+			if (preUpdate(delta)) {
+				update(delta); // This is the one that must be defined.
+				postUpdate(delta);
+			}
+			draw();
+		}
+	}
+
+	/**
+	 * Called when the Screen is paused.
+	 * 
+	 * This is usually when it's not active or visible on screen. An Application is 
+	 * also paused before it is destroyed.
+	 */
+	public void pause() {
+		// Left empty for menus, loading. Override for levels
+	}
+
+	/**
+	 * Called when the Screen is resumed from a paused state.
+	 *
+	 * This is usually when it regains focus.
+	 */
+	public void resume() {
+		// Left empty for menus, loading. Override for levels
+	}
+	
+	/**
+	 * Called when this screen becomes the current screen for a Game.
+	 */
+	public void show() {
+		// Useless if called in outside animation loop
+		active = true;
+	}
+
+	/**
+	 * Called when this screen is no longer the current screen for a Game.
+	 */
+	public void hide() {
+		// Useless if called in outside animation loop
+		active = false;
+	}
+
+	/**
+	 * Sets the ScreenListener for this mode
+	 *
+	 * The ScreenListener will respond to requests to quit.
+	 */
+	public void setScreenListener(ScreenListener listener) {
+		this.listener = listener;
+	}
+}
---
+++ MountSputnik/core/src/beigegang/mountsputnik/PauseMode.java
@@ -0,0 +1,23 @@
+package beigegang.mountsputnik;
+
+public class PauseMode extends ModeController {
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(float dt) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
