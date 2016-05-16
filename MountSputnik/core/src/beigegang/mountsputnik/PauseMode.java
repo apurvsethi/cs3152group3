59c1e3c93fa831c042b4d9f245c9466b5e7b91aa
Apurv Sethi <as2658@cornell.edu>
2016-05-16 13:13:59 -0400

    #21 add assets pause menu

--- MountSputnik/core/src/beigegang/mountsputnik/PauseMode.java
+++ MountSputnik/core/src/beigegang/mountsputnik/PauseMode.java
@@ -1,12 +1,15 @@
 package beigegang.mountsputnik;
 
-import beigegang.util.PooledList;
-import beigegang.util.ScreenListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader;
+
 import static beigegang.mountsputnik.Constants.*;
+import beigegang.util.ScreenListener;
 
 public class PauseMode extends ModeController {
 
@@ -15,20 +18,25 @@ public class PauseMode extends ModeController {
 	 */
 	protected AssetState assetState = AssetState.EMPTY;
 
-	private static final String FOREGROUND_FILE = "Menu/PauseMenu/Foreground.png";
-	private static final String MENU_OPTION_FILES[] = {"Menu/PauseMenu/Resume.png","Menu/PauseMenu/RestartLastCheckpoint.png", "Menu/PauseMenu/Restart.png","Menu/PauseMenu/Menu.png","Menu/PauseMenu/Quit.png"};
-	private static final String MENU_OPTION_FILES2[] = {"Menu/PauseMenu/Resume.png", "Menu/PauseMenu/Restart.png","Menu/PauseMenu/Menu.png","Menu/PauseMenu/Quit.png"};
-	private static boolean race = false;
+	private static final String GEOMETRIC_FONT_FILE = "Fonts/geometric.ttf";
+	private static BitmapFont geometricFontNormal;
+	private static BitmapFont geometricFontSelected;
+
+	private static final String OVERLAY_FILE = "Menu/Overlay.jpg";
+	private static final String TEXTBOX_FILE = "Menu/Text Box.png";
+
 	/**
 	 * Texture asset for files used, parts, etc.
 	 */
-	private static TextureRegion background;
-	private static TextureRegion foreground;
-	private static TextureRegion[] menuOptions = new TextureRegion[MENU_OPTION_FILES.length];
-	private static TextureRegion[] menuOptions2 = new TextureRegion[MENU_OPTION_FILES2.length];
+	private static TextureRegion overlay;
+	private static TextureRegion textbox;
 
+	private static boolean race = false;
+	private static String[] menuOptions = {"Resume", "Restart Last Checkpoint", "Restart Level", "Menu", "Quit"};
 	private static int exitCodes[] ={EXIT_GAME_RESUME_LEVEL,EXIT_GAME_RESTART_LAST_CHECKPOINT,EXIT_GAME_RESTART_LEVEL,EXIT_MENU,EXIT_QUIT};
+	private static String[] menuOptions2 = {"Resume", "Restart Race", "Menu", "Quit"};
 	private static int exitCodes2[] ={EXIT_GAME_RESUME_RACE_LEVEL,EXIT_GAME_RESTART_RACE_LEVEL,EXIT_MENU,EXIT_QUIT};
+	private static Color overlayColor = new Color(Color.WHITE);
 
 	private AssetManager assetManager;
 
@@ -36,23 +44,31 @@ public class PauseMode extends ModeController {
 	private int currSelection = 0;
 	private int changeCooldown = 0;
 
-	private PooledList<GameObject> gameObjects;
-
 	public void preLoadContent(AssetManager manager) {
 		assetManager = manager;
 		if (assetState != AssetState.EMPTY) return;
 
 		assetState = AssetState.LOADING;
-		manager.load(FOREGROUND_FILE, Texture.class);
-		assets.add(FOREGROUND_FILE);
-		for (String MENU_OPTION_FILE : MENU_OPTION_FILES) {
-			manager.load(MENU_OPTION_FILE, Texture.class);
-			assets.add(MENU_OPTION_FILE);
-		}
-		for (String MENU_OPTION_FILE : MENU_OPTION_FILES2) {
-			manager.load(MENU_OPTION_FILE, Texture.class);
-			assets.add(MENU_OPTION_FILE);
-		}
+
+		FreetypeFontLoader.FreeTypeFontLoaderParameter geometricNormal = new
+				FreetypeFontLoader.FreeTypeFontLoaderParameter();
+		geometricNormal.fontFileName = GEOMETRIC_FONT_FILE;
+		geometricNormal.fontParameters.size = (int)(45 * Gdx.graphics.getDensity());
+		geometricNormal.fontParameters.color = Color.BROWN;
+		assetManager.load("Normal" + GEOMETRIC_FONT_FILE, BitmapFont.class, geometricNormal);
+		assets.add("Normal" + GEOMETRIC_FONT_FILE);
+		FreetypeFontLoader.FreeTypeFontLoaderParameter geometricSelected = new
+				FreetypeFontLoader.FreeTypeFontLoaderParameter();
+		geometricSelected.fontFileName = GEOMETRIC_FONT_FILE;
+		geometricSelected.fontParameters.size = (int)(52 * Gdx.graphics.getDensity());
+		geometricSelected.fontParameters.color = Color.FIREBRICK;
+		assetManager.load("Selected" + GEOMETRIC_FONT_FILE, BitmapFont.class, geometricSelected);
+		assets.add("Selected" + GEOMETRIC_FONT_FILE);
+
+		manager.load(TEXTBOX_FILE, Texture.class);
+		assets.add(TEXTBOX_FILE);
+		manager.load(OVERLAY_FILE, Texture.class);
+		assets.add(OVERLAY_FILE);
 	}
 
 	/**
@@ -67,20 +83,17 @@ public class PauseMode extends ModeController {
 	public void loadContent(AssetManager manager) {
 		if (assetState != AssetState.LOADING) return;
 
-		foreground = createTexture(manager, FOREGROUND_FILE, false);
+		geometricFontNormal = manager.get("Normal" + GEOMETRIC_FONT_FILE, BitmapFont.class);
+		geometricFontSelected = manager.get("Selected" + GEOMETRIC_FONT_FILE, BitmapFont.class);
 
-		for (int i = 0; i < MENU_OPTION_FILES.length; i++) {
-			menuOptions[i] = createTexture(manager, MENU_OPTION_FILES[i], false);
-		}
-		for (int i = 0; i < MENU_OPTION_FILES2.length; i++) {
-			menuOptions2[i] = createTexture(manager, MENU_OPTION_FILES2[i], false);
-		}
+		overlay = createTexture(manager, OVERLAY_FILE, false);
+		textbox = createTexture(manager, TEXTBOX_FILE, false);
 		assetState = AssetState.COMPLETE;
 	}
 
 	@Override
 	public void reset() {
-		// TODO Auto-generated method stub
+		overlayColor.a = 0.3f;
 		currSelection = 0;
 		SoundController.get(SoundController.DECLINE_SOUND).play();
 	}
@@ -90,82 +103,47 @@ public class PauseMode extends ModeController {
 
 	public void update(float dt, ScreenListener listener,boolean r) {
 		race = r;
-		// TODO Auto-generated method stub
 		InputController input = InputController.getInstance(0);
 
 		if (changeCooldown > 0) changeCooldown --;
 
 		if (changeCooldown == 0 && Math.abs(input.getVerticalL()) > 0.5) {
 			SoundController.get(SoundController.SCROLL_SOUND).play();
-			if (race) currSelection = (currSelection + (input.getVerticalL() > 0.5 ? -1 : (input.getVerticalL() < -0.5 ? 1 : 0))+ menuOptions2.length) % menuOptions2.length;
-			else currSelection = (currSelection + (input.getVerticalL() > 0.5 ? -1 : (input.getVerticalL() < -0.5 ? 1 : 0))+ menuOptions.length) % menuOptions.length;
+			currSelection += input.getVerticalL() > 0.5 ? -1 : 1;
+			if (race) currSelection = (currSelection + menuOptions2.length) % menuOptions2.length;
+			else currSelection = (currSelection + menuOptions.length) % menuOptions.length;
 			changeCooldown = MENU_CHANGE_COOLDOWN;
+			if (race) changeCooldown *= 2;
 		}
 		if (input.didSelect()){
 			SoundController.get(SoundController.SELECT_SOUND).play();
-			if (race)
-				listener.exitScreen(this, exitCodes2[currSelection]);
-			else
-				listener.exitScreen(this, exitCodes[currSelection]);
+			if (race) listener.exitScreen(this, exitCodes2[currSelection]);
+			else listener.exitScreen(this, exitCodes[currSelection]);
 		}
 
 	}
 
 	public void draw(GameCanvas canvas) {
-
-		float y = canvas.getCamera().position.y - canvas.getHeight() / 2;
-
-//		canvas.begin();
-//		canvas.draw(background,Color.WHITE, canvas.getWidth() * 3 / 4, y,canvas.getWidth() / 4,canvas.getHeight());
-//		for (GameObject obj : gameObjects) obj.draw(canvas);
-//		canvas.end();
-//
-//		if (debug) {
-//			canvas.beginDebug();
-//			for(GameObject obj : gameObjects) obj.drawDebug(canvas);
-//			canvas.endDebug();
-//		}
-
 		canvas.begin();
-		canvas.draw(foreground, Color.WHITE, 0, y,canvas.getWidth(),canvas.getHeight());
-		canvas.end();
-		canvas.begin();
-		float drawY = canvas.getHeight() * 5f/11f + (MENU_ITEM_HEIGHT * menuOptions.length / 2) + y;
-		if (!race) {
-			for (int i = 0; i < menuOptions.length; i++) {
+		float bottomOfScreen = canvas.getCamera().position.y - canvas.getHeight() / 2;
+		float textboxWidth = race ? canvas.getWidth() * 0.35f : canvas.getWidth() * 0.5f;
+		float textboxHeight = race ? canvas.getHeight() * 0.42f : canvas.getHeight() * 0.5f;
+		canvas.draw(overlay, overlayColor, 0, bottomOfScreen, canvas.getWidth(), canvas.getHeight());
+		canvas.draw(textbox, Color.WHITE, (canvas.getWidth() - textboxWidth) / 2,
+				bottomOfScreen + (canvas.getHeight() - textboxHeight) / 2,
+				textboxWidth, textboxHeight);
+		String[] menu = race ? menuOptions2 : menuOptions;
+		BitmapFont font;
+		float drawY = bottomOfScreen;
+		drawY += race ? canvas.getHeight() * 0.09f : canvas.getHeight() * 0.117f;
 
-				if (i == currSelection) {
-					canvas.draw(menuOptions[i], Color.TEAL, menuOptions[i].getRegionWidth() / 2,
-							menuOptions[i].getRegionHeight() / 2, (canvas.getWidth() / 2), drawY, 0, 0.75f, 0.75f);
-				} else {
-					canvas.draw(menuOptions[i], Color.WHITE, menuOptions[i].getRegionWidth() / 2,
-							menuOptions[i].getRegionHeight() / 2, (canvas.getWidth() / 2), drawY, 0, 0.75f, 0.75f);
-				}
-				drawY -= MENU_ITEM_HEIGHT;
-			}
-		}else{
-			for (int i = 0; i < menuOptions2.length; i++) {
-				if (i == currSelection){
-					canvas.draw(menuOptions2[i], Color.TEAL, menuOptions2[i].getRegionWidth() / 2,
-							menuOptions2[i].getRegionHeight()/ 2, (canvas.getWidth() / 2), drawY, 0, 0.75f, 0.75f);
-				} else {
-					canvas.draw(menuOptions2[i], Color.WHITE,  menuOptions2[i].getRegionWidth() / 2,
-							menuOptions2[i].getRegionHeight()/ 2,(canvas.getWidth() / 2), drawY, 0, 0.75f, 0.75f);
-				}
-				drawY -= MENU_ITEM_HEIGHT;
-			}
+		for (int i = 0; i < menu.length; i++) {
+			font = currSelection == i ? geometricFontSelected : geometricFontNormal;
+			canvas.drawTextCentered(menu[i], font, drawY);
+			drawY -= canvas.getHeight() * 0.055;
 		}
-
-		//draw menu objects
 		canvas.end();
-
 	}
-
-	public void setGameObjects(PooledList<GameObject> objs){
-		gameObjects = objs;
-	}
-
-	public void setGameBackground(TextureRegion bck) { background = bck; }
 
 	@Override
 	public void resize(int width, int height) {
